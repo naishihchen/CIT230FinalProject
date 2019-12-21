51adc633783cc2e2944888f5de3fd2283c09c967
naishihchen <55642117+naishihchen@users.noreply.github.com>
2019-12-20 22:27:42 -0800

    Add files via upload

---
+++ js/temples.js
@@ -0,0 +1,28 @@
+const requestURL = 'https://naishihchen.github.io/js/temples.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const templedata = jsonObject['temples'];
+    for (let i = 0; i < templedata.length; i++ ) {
+      if (templedata[i].availability == "closed") {
+    	  let cardSection = document.createElement('section');
+        let card = document.createElement('div');
+		    let h2 = document.createElement('h3');
+        let closures = document.createElement('p');
+
+		    h2.textContent = "Closures";
+            closures.textContent = templedata[i].name + " - " + templedata[i].dates;
+
+    		card.appendChild(h2);
+        card.appendChild(closures);
+
+        cardSection.appendChild(card)
+      
+		    document.querySelector('div.cards').appendChild(cardSection);
+        }
+    	}
+  });
